9f5c5230406e588672cec7ecc1db4fb810382b56
ebartalucci <ettore.bartalucci@libero.it>
2024-09-10 16:25:46 +0200

    splitted db

--- src/noncov.py
+++ src/noncov.py
@@ -1755,14 +1755,14 @@ class MachineLearning(NONCOVToolbox):
     def __init__(self):
         super().__init__()
     
-    def make_empty_database(self, output_csv_path):
+    def make_empty_nuc_prop_df(self, output_csv_path):
         # Headers of features = number of columns
         columns = ['Molecule', 
                     'Atom', 
-                    'Noncov', 
                     'x_coord', 
                     'y_coord', 
                     'z_coord', 
+                    'sigma_iso',
                     'sigma_11', 
                     'sigma_22', 
                     'sigma_33', 
@@ -1771,10 +1771,32 @@ class MachineLearning(NONCOVToolbox):
                     'dia_sigma_33', 
                     'para_sigma_11', 
                     'para_sigma_22', 
-                    'para_sigma_33', 
-                    'sigma_iso', 
+                    'para_sigma_33',  
                     'nmr_functional', 
                     'nmr_basis_set', 
+                    ]
+        
+        # Create the dataframe
+        df = pd.DataFrame(columns=columns)
+        df_out = os.path.join(output_csv_path, 'nuc_prop_nmr_observables.csv')
+        
+        df.to_csv(df_out, index=False)
+        
+        # Keep the user happy by saying that something happened
+        print(f'The empty nuclear property dataset has been created and saved in: {df_out}')
+        print('\n')
+
+    def make_empty_pairwise_prop_df(self, output_csv_path):
+        # Headers of features = number of columns
+        columns = ['Molecule', 
+                    'Atom_1', 
+                    'Atom_2',
+                    'x_coord_1', 
+                    'y_coord_1', 
+                    'z_coord_1', 
+                    'x_coord_2', 
+                    'y_coord_2', 
+                    'z_coord_2',
                     'J_iso',
                     'J_FC_11',
                     'J_FC_22',
@@ -1788,205 +1810,18 @@ class MachineLearning(NONCOVToolbox):
                     'J_SD_11',
                     'J_SD_22',
                     'J_SD_33',
-                    'Mayer_BO'
-                        ]
+                    'Mayer_BO' 
+                    ]
         
         # Create the dataframe
         df = pd.DataFrame(columns=columns)
-        df_out = os.path.join(output_csv_path, 'calculated_nmr_observables.csv')
+        df_out = os.path.join(output_csv_path, 'pairwise_nuc_prop_nmr_observables.csv')
         
         df.to_csv(df_out, index=False)
         
         # Keep the user happy by saying that something happened
-        print(f'The empty dataset has been created and saved in: {df_out}')
+        print(f'The empty pairwise nuclear property dataset has been created and saved in: {df_out}')
         print('\n')
-
-    # Extract features from splitted orca output files
-    def extract_data_for_ml_database(self, file_path):
-        """
-        Read the splitted output file from an ORCA calculation and features for machine learning.
-        Input
-        :param file_path: output file from orca in the form .out
-
-        Output: data dictionary containing
-        :molecule
-        :atom
-        :noncov
-        :x_coord
-        :y_coord
-        :z_coord
-        :tot_shielding
-        :dia_shielding
-        :para_shielding
-        :iso_shift
-        :nmr_functional
-        :nmr_basis_set
-        :aromatic
-        """
-        # Define empty feature variables to extract and append to dataset
-        # All shielding tensors are diagonalized in PAS and ordered in the Mehring way before being appended to database
-        data = {
-            'molecule': [],
-            'atom': [],
-            'noncov': [],
-            'x_coord': [],
-            'y_coord': [],
-            'z_coord': [],
-            'tot_shielding_11': [],
-            'tot_shielding_22': [],
-            'tot_shielding_33': [],
-            'dia_shielding_11': [],
-            'dia_shielding_22': [],
-            'dia_shielding_33': [],
-            'para_shielding_11': [],
-            'para_shielding_22': [],
-            'para_shielding_33': [],
-            'iso_shift': [],
-            'nmr_functional': [],
-            'nmr_basis_set': [],
-            'aromatic': []
-        }
-
-
-        try:
-            with open(file_path, 'r') as f:
-                lines = f.readlines()
-                
-                # Search markers
-                coordinates_found = False
-                shielding_found = False
-                molecule_found = False
-                noncov_found = False
-                aromatic_found = False
-
-                current_dia_shielding = []
-                current_para_shielding = []
-                current_tot_shielding = []
-
-                for line in lines:
-                    # @UserStaticInputs
-                    # Get molecular information from the user-defined Molecule flag in the input file
-                    if '# Molecule:' in line:
-                        molecule_found = True
-                        data['molecule'].append(line.split(':')[-1].strip())
-                    
-                    # Get Noncov type information from the user-defined Noncov flag in the input file
-                    elif '# Noncov:' in line:
-                        noncov_found = True
-                        data['noncov'].append(line.split(':')[-1].strip())
-                    
-                    # Get aromatic information from the user-defined Aromatic flag in the input file
-                    elif '# Aromatic:' in line:
-                        aromatic_found = True
-                        data['aromatic'].append(line.split(':')[-1].strip()) # as binary 1 or 0
-
-                    # To check, maybe get these info from shielding files?
-                    # Get atom and relative coordinates info from file
-                    elif 'CARTESIAN COORDINATES (ANGSTROEM)' in line:
-                        coordinates_found = True
-                    
-                    # add a linethat add numbers to nuclei to match
-                    elif coordinates_found and line.strip():
-                        atomic_info = line.split()
-                        data['atom'].append(atomic_info[0])
-                        data['x_coord'].append(float(atomic_info[1]))
-                        data['y_coord'].append(float(atomic_info[2]))
-                        data['z_coord'].append(float(atomic_info[3]))
-                    
-                    # Get the total, diamagnetic and paramagnetic tensors and diagonalize them
-                    elif 'CHEMICAL SHIFTS' in line:
-                        shielding_found = True
-                        continue
-                    
-                    if shielding_found:
-                        line = line.strip()
-
-                        if line.startswith('Diamagnetic contribution to the shielding tensor (ppm) :'):
-                            try:
-                                dia_tensor_components = [float(x) for x in line.split()[1:4]]
-                                current_dia_shielding.append(dia_tensor_components)
-                            except (ValueError, IndexError):
-                                continue
-
-                        elif line.startswith('Paramagnetic contribution to the shielding tensor (ppm):'):
-                            try:
-                                para_tensor_components = [float(x) for x in line.split()[1:4]]
-                                current_para_shielding.append(para_tensor_components)
-                            except (ValueError, IndexError):
-                                continue
-
-                        elif line.startswith('Total shielding tensor (ppm):'):
-                            try:
-                                tot_tensor_components = [float(x) for x in line.split()[1:4]]
-                                current_tot_shielding.append(tot_tensor_components)
-                            except (ValueError, IndexError):
-                                continue
-
-                # Diagonalize the tensors
-                if current_dia_shielding:
-                    data['dia_shielding_11'], data['dia_shielding_22'], data['dia_shielding_33'] = NMRFunctions.diagonalize_tensor(
-                        *current_dia_shielding)
-                if current_para_shielding:
-                    data['para_shielding_11'], data['para_shielding_22'], data['para_shielding_33'] = NMRFunctions.diagonalize_tensor(
-                        *current_para_shielding)
-                if current_tot_shielding:
-                    data['tot_shielding_11'], data['tot_shielding_22'], data['tot_shielding_33'] = NMRFunctions.diagonalize_tensor(
-                        *current_tot_shielding)
-                            
-            
-                # Get functional and basis set for NMR calculations from the line containing the Level of theory flag
-                for i, line in enumerate(lines):
-                    if "# Level of theory" in line:
-                        # Extract the line immediately after it, this wont work if ppl dont use my syntax
-                        level_of_theory_line = lines[i + 1].strip()
-
-                        # Remove the line number from the line - do i want to keep this?
-                        level_of_theory = level_of_theory_line.replace("| 10> !", "").strip()
-                        level_of_theory = level_of_theory.split()
-
-                        # Extract functional and basis set
-                        data['nmr_functional'] = level_of_theory[0]
-                        data['nmr_basis_set'] = level_of_theory[1]
-                
-                if not molecule_found and not noncov_found and not aromatic_found:
-                    data['molecule'] = [0]
-                    data['noncov'] = [0]  
-                    data['aromatic'] = [0] 
-                    
-        except FileNotFoundError:
-            return f"File '{file_path}' not found."
-        except Exception as e:
-            return f"An error occurred: {str(e)}"
-        
-        return data
-
-
-    # Search for all the splitted output files from an ORCA calculation in the Machine learning project root directory
-    def search_files(self):
-        # Iterate through all directories and subdirectories in root for the orca output files
-        for root, dirs, files in os.walk(self.root_directory):
-            for file in files:
-                if file.startswith('splitted_') and file.endswith('.out'): # working with output files is much easier than with full mpi8.out
-                    # get the path to those files
-                    file_path = os.path.join(root, file)
-
-                    instance_data = self.extract_data_for_ml_database(file_path)
-                    
-                    # Check if the instance data is not empty (indicating an error)
-                    #if instance_data:
-                        # Construct DataFrame from the instance data dictionary
-                        #instance_df = pd.DataFrame(instance_data)
-                        
-                        # Append the DataFrame to the main DataFrame
-                        #self.df = self.df.append(instance_df, ignore_index=True)
-                        
-                        # Write to CSV file
-                        #self.df.to_csv(self.output_csv_path, index=False)
-                    #else:
-                        #print(f"No data extracted from file: {file_path}")
-
-        if self.df.empty:
-            print("No raw data has been found matching the specified criteria.")
 
 # ------------------------------------------------------------------------------
 #                       DESCRIPTORS AND FEATURE SELECTION
